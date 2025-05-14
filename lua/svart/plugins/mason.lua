da3516c4a1e962a55cbfcd5d3faa55a0c9154f9b
Victor Moraes <victormoraespt4@gmail.com>
2025-05-14 01:34:25 -0300

    mason config

--- lua/svart/plugins/mason.lua
+++ lua/svart/plugins/mason.lua
@@ -1,25 +1,35 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    require("mason").setup()
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup()
 
-    require("mason-lspconfig").setup({
-      automatic_installation = true,
-      ensure_installed = {
-        "gopls",
-        "basedpyright",
-      },
-    })
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"gopls",
+				"basedpyright",
+				"docker-compose-language-service",
+				"dockerfile-language-server",
+				"emmet-language-server",
+				"goimports",
+				"json-lsp",
+				"lua-language-server",
+				"prettier",
+				"pylint",
+				"stylua",
+				"typescript-language-server",
+			},
+		})
 
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "stylua",
-        "pylint",
-      },
-    })
-  end,
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"stylua",
+				"pylint",
+			},
+		})
+	end,
 }
