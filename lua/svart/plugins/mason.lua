45b48530a7995c2d888a14bdc4d90bcc454bd0ad
Victor Moraes <victormoraespt4@gmail.com>
2025-05-15 01:51:47 -0300

    bugfix

--- lua/svart/plugins/mason.lua
+++ lua/svart/plugins/mason.lua
@@ -1,35 +1,35 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		require("mason").setup()
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup()
 
-		require("mason-lspconfig").setup({
-			automatic_installation = true,
-			ensure_installed = {
-				"gopls",
-				"basedpyright",
-				"docker-compose-language-service",
-				"dockerfile-language-server",
-				"emmet-language-server",
-				"goimports",
-				"json-lsp",
-				"lua-language-server",
-				"prettier",
-				"pylint",
-				"stylua",
-				"typescript-language-server",
-			},
-		})
+    -- LSP servers (usam nomes do lspconfig)
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      ensure_installed = {
+        "gopls",
+        "basedpyright",
+        "docker_compose_language_service",
+        "dockerls",
+        "emmet_language_server",
+        "jsonls",
+        "lua_ls",
+        "ts_ls",
+      },
+    })
 
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"stylua",
-				"pylint",
-			},
-		})
-	end,
+    -- Ferramentas (formatadores e linters, usam nomes do mason registry)
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "goimports",
+        "prettier",
+        "pylint",
+        "stylua",
+      },
+    })
+  end,
 }
